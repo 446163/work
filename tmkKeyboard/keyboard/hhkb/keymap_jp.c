b8f27b0e12b66ef809b384f90faa40c104868026
446163 <dan0qn@hotmail.co.uk>
2016-11-02 06:42:46 +0000

    auto save

--- tmkKeyboard/keyboard/hhkb/keymap_jp.c
+++ tmkKeyboard/keyboard/hhkb/keymap_jp.c
@@ -34,6 +34,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * FN 3 = switch to custom shift layer <><><><> NO SHIFT <><><><>
      * FN 4 = mouse layer
      * FN 5 = macro layer
+     * FN 6 = default layer
      *
      * Layer 0 = default esc/bskp
      * Layer 1 = HHKB mode / layer layer
@@ -47,7 +48,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     KEYMAP_JP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
               CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, \
               FN5,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,PENT, \
-              FN2,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,RSFT,TRNS, \
+              FN2,FN6,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,RSFT,TRNS, \
               TRNS,FN1,FN4,TRNS,TRNS,     TRNS,     TRNS,TRNS,TRNS,TRNS,DEL,RGUI,RCTL),
 
     KEYMAP_JP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, JYEN,BSPC, \
